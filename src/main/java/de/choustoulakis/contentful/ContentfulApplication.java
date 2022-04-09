8cf5821013fa1fa076bb3354c6eedbebaef8bf57
choustni <nikolaos@choustoulakis.de>
2022-04-09 13:18:13 +0300

    first commit

---
+++ src/main/java/de/choustoulakis/contentful/ContentfulApplication.java
@@ -0,0 +1,13 @@
+package de.choustoulakis.contentful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/choustoulakis/contentful/ContentfulApplicationTests.java
@@ -0,0 +1,13 @@
+package de.choustoulakis.contentful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
