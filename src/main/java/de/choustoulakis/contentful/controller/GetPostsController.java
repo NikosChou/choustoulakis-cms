70248166a28293a332115680030579654e296481
choustni <nikolaos@choustoulakis.de>
2022-04-12 18:43:37 +0300

    log entry

--- src/main/java/de/choustoulakis/contentful/controller/GetPostsController.java
+++ src/main/java/de/choustoulakis/contentful/controller/GetPostsController.java
@@ -3,6 +3,7 @@ package de.choustoulakis.contentful.controller;
 import de.choustoulakis.contentful.model.Posts;
 import de.choustoulakis.contentful.service.ContentfulService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Log4j2
 @RequestMapping("/posts")
 @AllArgsConstructor
 @Controller
@@ -20,6 +22,7 @@ public class GetPostsController {
 
   @GetMapping(value = "/homepage", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Posts> getHomepagePosts() {
+    log.info("Entering homepage");
     var posts =
         Posts.builder().data(this.service.getPosts(List.of("homepage-2", "homepage-1"))).build();
     return ResponseEntity.ok(posts);
