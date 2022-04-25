d45c462194d8a11271feacbbb27c6aeb45977137
choustni <nikolaos@choustoulakis.de>
2022-04-25 08:51:40 +0300

    fix test

--- src/test/java/de/choustoulakis/contentful/configuration/StaticContentRouteConfigurationTest.java
+++ src/test/java/de/choustoulakis/contentful/configuration/StaticContentRouteConfigurationTest.java
@@ -1,8 +1,11 @@
 package de.choustoulakis.contentful.configuration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -10,10 +13,28 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(classes = {StaticContentRouteConfiguration.class})
 class StaticContentRouteConfigurationTest {
 
-  @Autowired private WebTestClient webClient;
+  @Autowired private StaticContentRouteConfiguration staticContentRouteConfiguration;
+
+  private WebTestClient webClient;
+
+  @BeforeEach
+  void setUp() {
+    this.webClient =
+        WebTestClient.bindToRouterFunction(staticContentRouteConfiguration.staticRoute()).build();
+  }
 
   @Test
-  void foo() {
-    this.webClient.get().uri("/static/choustoulakis_cv.pdf").exchange().expectStatus().isOk();
+  void shouldServeStaticResource() {
+    this.webClient
+        .get()
+        .uri("/static/choustoulakis_cv.pdf")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_PDF)
+        .expectHeader()
+        .contentLength(55336)
+        .expectBody(Resource.class);
   }
 }
